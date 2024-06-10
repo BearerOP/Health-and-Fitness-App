63c3709540e23d04543443baccc2be6c6269af8d
BearerOP <ankity1892003@gmail.com>
2024-06-10 09:06:25 +0530

    reminder implementation and env updated

--- src/services/notification.js
+++ src/services/notification.js
@@ -5,9 +5,19 @@ const path = require("path");
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      require(path.resolve(__dirname, "../../serviceAccountKey.json"))
-    ),
+    credential: admin.credential.cert({
+      type: process.env.GC_TYPE,
+      project_id: process.env.GC_PROJECT_ID,
+      private_key_id: process.env.GC_PRIVATE_KEY_ID,
+      private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.GC_CLIENT_EMAIL,
+      client_id: process.env.GC_CLIENT_ID,
+      auth_uri: process.env.GC_AUTH_URI,
+      token_uri: process.env.GC_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.GC_CLIENT_X509_CERT_URL,
+      universe_domain: process.env.UNIVERSE_DOMAIN,
+    }),
   });
 }
 
